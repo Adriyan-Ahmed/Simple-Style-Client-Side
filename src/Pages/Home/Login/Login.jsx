db24e828401509616126a208cc24a763ad8e4071
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-02-21 17:43:58 +0600

    Authentication Method Created

--- src/Pages/Home/Login/Login.jsx
+++ src/Pages/Home/Login/Login.jsx
@@ -5,7 +5,7 @@ import LoginForm from '../../../Components/Login Form/LoginForm';
 const Login = () => {
     return (
         <>
-            <section className='container mx-auto'>
+            <section className='container mx-auto pt-20'>
                 <LoginForm></LoginForm>
             </section>
         </>
