6289d8c26139edc62e72e1cc0018a2920c497cc3
Adriyan-Ahmed <adriyan.ahmed.jubayer@gmail.com>
2024-02-22 10:13:00 +0600

    Newsletter Section Created

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -3,15 +3,16 @@ const Subscribe = () => {
         <>
             <div className="container mx-auto">
                 <div className="mx-4">
-                    <div className="bg-[#A60FEC] bg-opacity-15">
+                    <div className="bg-[#A60FEC] bg-opacity-15 md:py-28 md:px-14 xl:p-36 rounded-lg">
+                        <h1 className="text-2xl md:text-[23px] lg:text-3xl xl:text-4xl xl:leading-normal font-bold mt-4 mb-6 w-max mx-auto ">Subscribe Now to Our <span className="text-[#A60FEC]">Newsletter!</span></h1>
                         <form>
-                            <div className="flex items-center w-full">
-                                <div className="relative input-box">
-                                    <input className="border-2 w-full py-2.5 rounded-md px-5 outline-none" type="email" name="email" required />
+                            <div className="flex items-center xl:w-7/12 mx-auto">
+                                <div className="relative input-box w-full">
+                                    <input className="border-l-2 border-t-2 border-b-2 w-full py-2.5 rounded-tl-md rounded-bl-md px-5 outline-none" type="email" name="email" required />
                                     <span className="absolute left-3 text-gray-400 duration-300 ">YOUR E-MAIL</span>
                                 </div>
                                 <div>
-                                    <button className="bg-[#0FCFEC] rounded-md text-white font-bold py-3 px-12 text-sm md:text-base hover:bg-transparent hover:py-2.5 hover:border-[#0FCFEC] hover:border-2 hover:text-[#0FCFEC] duration-500 bg-opacity-90"></button>
+                                    <button className="bg-[#A60FEC] rounded-tr-md rounded-br-md text-white font-bold py-3 px-12 text-sm md:text-base hover:bg-transparent hover:py-2.5 hover:border-[#0FCFEC] hover:border-2 hover:text-[#0FCFEC] duration-500 bg-opacity-90">SUBSCRIBE</button>
                                 </div>
                             </div>
                         </form>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import AboutUs from "../../Components/About Us/AboutUs";
 import Banner from "../../Components/Banner/Banner";
 import ContactUs from "../../Components/Contact Us/ContactUs";
 import Products from "../../Components/Products/Products";
+import Subscribe from "../../Components/Subscribe/Subscribe";
 import Testimonial from "../../Components/Testimonial/Testimonial";
 import WhyUs from "../../Components/Why Us/WhyUs";
 import SectionHead from "../../Shared/Section Head/SectionHead";
@@ -26,6 +27,9 @@ const Home = () => {
             </section>
             <section>
                 <ContactUs></ContactUs>
+            </section>
+            <section>
+                <Subscribe></Subscribe>
             </section>
         </>
     );
